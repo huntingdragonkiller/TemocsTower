b4dafdcf3a57fd2cf62a7846d56335f7e1e76f42
lemon <jackson.w.lam@gmail.com>
2025-03-13 22:17:56 -0500

    Basic Enemy stats
    - Scriptable objects for enemy stats that can be reused to make new enemy types
    - Enemy Stats script translates the scriptable objects data
    - Enemy AI left blank to build off of pulling stat datas

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public EnemyScriptableObject enemyData;
+    
+    //Current stats
+    [HideInInspector]
+    public float currentMoveSpeed;
+    [HideInInspector]
+    public float currentHealth;
+    [HideInInspector]
+    public float currentDamage;
+    
+    void Awake()
+    {
+        currentMoveSpeed = enemyData.MoveSpeed;
+        currentHealth = enemyData.MaxHealth;
+        currentDamage = enemyData.Damage;
+    }
+    
+    public void TakeDamage(float dmg)
+    {
+        currentHealth -= dmg;
+        if (currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
